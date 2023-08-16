66eb6b0b57e92e968aece1e8dd284d3698a3ad53
Patrick Williams <pjwilliams2@users.noreply.github.com>
2023-08-16 12:38:18 -0400

    fix: version install script (#21)
    Co-authored-by: Patrick Williams <patrickwilliams@quickenloans.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -19,13 +19,14 @@ fi
 # check if CircleCI CLI needs to be installed
 
 if command -v circleci > "$OUTPUT" 2>&1; then
-    if circleci version | grep "${CIRCLECI_CLI_VERSION:1}" > /dev/null 2>&1; then
+    VERSION="${CIRCLECI_CLI_VERSION:1}"
+    if circleci version | grep "${VERSION}" > /dev/null 2>&1; then
         echo "circleci $CIRCLECI_CLI_VERSION is already installed"
         exit 0
     else
         echo "A different version of the CircleCI CLI is installed ($(circleci version)); updating it"
         $SUDO rm -f "$(which circleci)"
-        curl -fLSs https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh | $SUDO bash
+        curl -fLSs https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh | $SUDO VERSION="$VERSION" bash
         echo "CircleCI CLI version $(circleci --skip-update-check version) has been installed to $(which circleci)"
     fi
 fi
